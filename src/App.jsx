98af144e247c5b39552be08377a61b3ff09733c8
Jimmy <jimmy.ganci@gmail.com>
2022-05-11 10:43:51 +0200

    added loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { Canvas } from '@react-three/fiber';
 import Carrera from './components/Carrera';
-import { Environment, OrbitControls } from '@react-three/drei';
+import { Environment, Loader, OrbitControls } from '@react-three/drei';
 import MovingSpots from './components/MoveSpot';
 // import CameraRig from './components/CameraRig';
 
 function App() {
   return (
-    <Canvas gl={false}>
-      <ambientLight intensity={0.7} />
-      <spotLight position={[0, 30, 0]} angle={0.3} />
-      <OrbitControls
-        enablePan={false}
-        enableZoom={false}
-        minPolarAngle={Math.PI / 2.4}
-        maxPolarAngle={Math.PI / 2.4}
-      />
-      <Environment frames={Infinity} resolution={256} preset='warehouse'>
-        <MovingSpots />
-      </Environment>
-      <Carrera scale={1} rotation={[0, Math.PI / 3, 0]} />
-    </Canvas>
+    <>
+      <Canvas gl={false}>
+        <ambientLight intensity={0.7} />
+        <spotLight position={[0, 30, 0]} angle={0.3} />
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          minPolarAngle={Math.PI / 2.4}
+          maxPolarAngle={Math.PI / 2.4}
+        />
+        <Environment frames={Infinity} resolution={256} preset='warehouse'>
+          <MovingSpots />
+        </Environment>
+        <Carrera scale={1} rotation={[0, Math.PI / 3, 0]} />
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
